7a1dcd9e56b8f56362f4ac8d8d2e710aabf6cf1a
Harshal Lakhani <107047750+HarshalLakhani@users.noreply.github.com>
2024-02-01 22:20:50 +0530

    commit 1

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+Using namespace std;
+
+int main() {
+    // Declare variables
+    int a, b, c;
+
+    // Input for variable a
+    cout << "Enter the value of a: ";
+     cin >> a;
+
+    // Input for variable b
+     cout << "Enter the value of b: ";
+     cin >> b;
+
+    // Addition of a and b
+    c = a + b;
+
+    // Output the result
+     cout << "The sum of a and b is: " << c <<  endl;
+
+    return 0;
+}
